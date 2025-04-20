9f22983d30094a76988be5f36ff03769f54afd84
yichangshin <ycs318@naver.com>
2025-04-20 19:34:24 +0900

    프로젝트 최초 업로드

---
+++ src/main/java/com/eip/eip/EipApplication.java
@@ -0,0 +1,13 @@
+package com.eip.eip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EipApplication.class, args);
+	}
+
+}
